c39595e906b1046aa1bb59b75b1a228132ce496f
Leandro Acquati <leandroacquati@gmail.com>
2021-03-05 01:19:50 -0300

    add Countdown component

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.sass'
 export default function CompletedChallenges() {
   return (
     <div className={styles.completedChallengesContainer}>
-      <span>Desafion Completos</span>
+      <span>Desafios Completos</span>
       <span>5</span>
     </div>
   )
